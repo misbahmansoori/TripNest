aff152799096afa99b833dd54e8b6116cee78923
Misbah Mansoori <misbahmansoori14@gmail.com>
2025-07-02 23:27:31 +0530

    Removed node_modules from Git tracking

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ store.on("error", ()=> {
 
 const sessionOptions = {
     store,
-    secret: process.enc.SECRET,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
